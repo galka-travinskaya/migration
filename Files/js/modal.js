294b26d7f2b23377cd2b72ace93bad84a8068f93
galka-travinskaya <66030379+galka-travinskaya@users.noreply.github.com>
2024-11-21 15:06:39 +0200

    add pages

--- Files/js/modal.js
+++ Files/js/modal.js
@@ -1,29 +1,15 @@
-// (() => {
-//     const refs = {
-//       openModalBtn: document.querySelector('[data-modal-open]'),
-//       closeModalBtn: document.querySelector('[data-modal-close]'),
-//       modal: document.querySelector('[data-modal]'),
-//     };
-  
-//     refs.openModalBtn.addEventListener('click', toggleModal);
-//     refs.closeModalBtn.addEventListener('click', toggleModal);
-  
-//     function toggleModal() {
-//       refs.modal.classList.toggle('is-hidden');
-//     }
-//   })();
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
 
-  
-  (() => {
-    document.querySelector('.js-form').addEventListener('submit', e => {
-      e.preventDefault();
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
-      console.log('');
-      console.count('Submit #');
-
-      new FormData(e.currentTarget).forEach((value, name) =>
-        console.log(`${name}: ${value}`),
-      );
-    });
-  })();
- 
+  function toggleModal() {
+    document.body.classList.toggle('modal-open')
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
