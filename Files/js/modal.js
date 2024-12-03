7b808f37594a0cde37545ec8314c5ed888fab075
galka-travinskaya <66030379+galka-travinskaya@users.noreply.github.com>
2024-12-03 13:41:32 +0200

    add js for modal

--- Files/js/modal.js
+++ Files/js/modal.js
@@ -1,73 +1,3 @@
-// (() => {
-//   const refs = {
-//     openModalBtn: document.querySelector('[data-modal-open]'),
-//     closeModalBtn: document.querySelector('[data-modal-close]'),
-//     modal: document.querySelector('[data-modal]'),
-//   };
-
-//   refs.openModalBtn.addEventListener('click', toggleModal);
-//   // refs.closeModalBtn.addEventListener('click', toggleModal);
-
-//   function toggleModal() {
-//     document.body.classList.toggle('modal-open')
-//     // refs.modal.classList.toggle('is-hidden');
-//   };
-
-//   document.querySelector('.btn-submit').addEventListener('click', () => {
-//     const name = document.getElementById('name').value;
-//     const email = document.getElementById('email').value;
-//     const phone = document.getElementById('tel').value;
-//     const data = { name, phone, email };
-  
-//     try {
-//         const response = fetch('https://shop-back-mh7t.onrender.com/api/client', {
-//             method: 'POST',
-//             headers: {
-//                 'Accept': 'application/json',
-//                 'Content-Type': 'application/json',
-//             },
-//             body: JSON.stringify(data),
-//         });
-//     } catch (error) {
-//         console.log(error);
-//     }
-//   })
-// })();
-
-
-
-// const btnSubmitEl = document.querySelector(".btn-submit[data-modal-close]");
-// const btnCloseModal = document.querySelector(".icon-close[data-modal-close]");
-
-
-// btnSubmitEl.addEventListener('click', function () {
-      
-
-//     const name = document.getElementById('name').value;
-//     const email = document.getElementById('email').value;
-//     const phone = document.getElementById('tel').value;
-//     const data = { name, phone, email };
-  
-//     try {
-//         const response = fetch('https://shop-back-mh7t.onrender.com/api/client', {
-//             method: 'POST',
-//             headers: {
-//                 'Accept': 'application/json',
-//                 'Content-Type': 'application/json',
-//             },
-//             body: JSON.stringify(data),
-//         });
-//     } catch (error) {
-//         console.log(error);
-//     };
-
-//     console.log('submit'); 
-//   });
-
-// btnCloseModal.addEventListener('click', function () {
-//     console.log('close');
-    
-// })
 
 (() => {
   const refs = {
@@ -86,27 +16,39 @@
     refs.modal.classList.toggle('is-hidden');
   };
 
-  document.querySelector('.btn-submit').addEventListener('click', () => {
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const phone = document.getElementById('tel').value;
-    const data = { name, phone, email };
-  
-    try {
-        const response = fetch('https://shop-back-mh7t.onrender.com/api/client', {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data),
-        });
-    } catch (error) {
-        console.log(error);
-    }
+  const countries = document.querySelectorAll('input[name="country"]');
 
-    
-  })
+    let selectedCountries = [];
+
+    countries.forEach(elem => {
+        elem.addEventListener('change', () => {
+            selectedCountries = Array.from(countries)
+                .filter(item => item.checked)
+                .map(box => box.value);
+        });
+    });
+
+    document.querySelector('.btn-submit').addEventListener('click', () => {
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const phone = document.getElementById('tel').value;
+
+        const data = { name, phone, email, country: selectedCountries };
+
+        try {
+            const response = fetch('https://shop-back-mh7t.onrender.com/api/client', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    })
 })();
+    
 
 
